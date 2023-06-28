dace0ad6dfc49f6d3f8e1f79418c61e21968c884
Dattatreya Pujar <62793249+dattdgp@users.noreply.github.com>
2023-06-28 13:01:04 +0530

    Update test.sh

--- ARMTemplates/test.sh
+++ ARMTemplates/test.sh
@@ -3,5 +3,5 @@ echo "This is displayed"
 echo "creating the resourge group..."
 az group create --name wincc-rg1 --location eastus
 az extension add --name azure-iot
-az iot hub create --name demoHub-01 --resource-group wincc-rg --sku S1
+az iot hub create --name demoHub-01 --resource-group wincc-rg1 --sku S1
 az iot hub device-identity create -n demoHub-01 -d dgp-wincc --am x509_ca --status enabled --status-reason 'for testing'
