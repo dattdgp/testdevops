8304d622ea367e257f3dc5c68e96550f8fc9f737
Dattatreya Pujar <62793249+dattdgp@users.noreply.github.com>
2023-06-28 14:27:32 +0530

    Update test.sh

--- ARMTemplates/test.sh
+++ ARMTemplates/test.sh
@@ -1,11 +1,11 @@
 $resourceGroup = Read-Host -Prompt "Enter your resource group name: "
 echo "creating the resourge group $resourceGroup..."
-az group create --name wincc-rg --location eastus
+az group create --name $resourceGroup --location eastus
 echo "Resource group $resourceGroup is created."
 echo "Adding IOT Hub extension..."
 az extension add --name azure-iot
 $iothub = Read-Host -Prompt "Enter your unique IOT Hub name: "
-echo "Creating Azure IOT Hub --> $iothub"
+echo "Creating Azure IOT Hub --> $iothub with S1 SKU"
 az iot hub create --name $iothub --resource-group $resourceGroup --sku S1
 $deviceID = Read-Host -Prompt "Enter the device ID to be added to $iothub hub: "
 echo "Adding IoT device $deviceID to the IoT hub $iothub with authentication type as x509 CA signed..."
