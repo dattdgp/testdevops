dbceefca4f31e02ae88ceeeedc4b70742fba52bc
Dattatreya Pujar <62793249+dattdgp@users.noreply.github.com>
2023-06-28 14:25:27 +0530

    Update test.sh

--- ARMTemplates/test.sh
+++ ARMTemplates/test.sh
@@ -1,7 +1,12 @@
-echo "This is displayed"
-#echo "This is NOT"
-echo "creating the resourge group..."
-az group create --name wincc-rg1 --location eastus
+$resourceGroup = Read-Host -Prompt "Enter your resource group name: "
+echo "creating the resourge group..." $resourceGroup
+az group create --name wincc-rg --location eastus
+echo "Resource group $resourceGroup is created."
+echo "Adding IOT Hub extension..."
 az extension add --name azure-iot
-az iot hub create --name demoHub-01 --resource-group wincc-rg1 --sku S1
-az iot hub device-identity create -n demoHub-01 -d dgp-wincc --am x509_ca --status enabled --status-reason 'for testing'
+$iothub = Read-Host -Prompt "Enter your unique IOT Hub name: "
+echo "Creating Azure IOT Hub --> $iothub"
+az iot hub create --name $iothub --resource-group $resourceGroup --sku S1
+$deviceID = Read-Host -Prompt "Enter the device ID to be added to $iothub hub: "
+echo "Adding IoT device $deviceID to the IoT hub $iothub with authentication type as x509 CA signed..."
+az iot hub device-identity create -n $iothub -d $deviceID --am x509_ca --status enabled --status-reason 'for testing'
